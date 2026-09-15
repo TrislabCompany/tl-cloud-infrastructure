81e592d15acf6415d35408bd0f83fe1a77753a99
tl-janskof <jan.skof@trislab.com>
2026-09-15 15:09:53 +0200

    Phase 5 (Azure): add Trislab-DevOps Entra ID group (#11)

--- shared-infrastructure/azure/providers.tf
+++ shared-infrastructure/azure/providers.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.0"
+    }
   }
 }
 
@@ -12,3 +16,5 @@ provider "azurerm" {
   features {}
   subscription_id = var.azure_subscription_id
 }
+
+provider "azuread" {}
